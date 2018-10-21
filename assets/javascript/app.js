f21691132089540ac5d17250141a69e61318b3f8
Chad Cook <chadcookcoding@gmail.com>
2018-10-21 09:14:59 -0400

    added images to use. html framework with background div and header styling. imported fonts

---
+++ assets/javascript/app.js
@@ -0,0 +1,21 @@
+// in what year will the next halloween with full moon occur
+// 2020
+// in what country was the first account of children saying "truck or treat" on halloween
+// canada
+// in what decade did trick-or-treating start gaining popuarity in US
+// 1930s
+// how many children have been seriously injured or killed from poisoned candy received from trick-or-treating=?
+// zero
+// what were jack o'lanterns originally made from
+// turnips
+// what country started the jack o'lantern carvinngs originate?
+// ireland
+// pumpkins are native to what continent?
+// north america
+// how much are american consumers expected to spend on halloween in 2018?
+// 9 billion dollars
+// what is the second most popular halloween candy?
+// candy corn
+// in what state is it illegal to dress up like a priest or nun?
+// alabama
+
