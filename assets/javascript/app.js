7d6b929a5b511ebde1e2ae203ce612f331737317
Chad Cook <chadcookcoding@gmail.com>
2018-10-21 20:46:55 -0400

    fixed some main style issues. Created question objects

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,102 @@
-// in what year will the next halloween with full moon occur
-// 2020
-// in what country was the first account of children saying "truck or treat" on halloween
-// canada
-// in what decade did trick-or-treating start gaining popuarity in US
-// 1930s
-// how many children have been seriously injured or killed from poisoned candy received from trick-or-treating=?
-// zero
-// what were jack o'lanterns originally made from
-// turnips
-// what country started the jack o'lantern carvinngs originate?
-// ireland
-// pumpkins are native to what continent?
-// north america
-// how much are american consumers expected to spend on halloween in 2018?
-// 9 billion dollars
-// what is the second most popular halloween candy?
-// candy corn
-// in what state is it illegal to dress up like a priest or nun?
-// alabama
-
+var trivia = {
+    q1: {
+        question: "In what year will the next halloween occur with a full moon?",
+        answers: {
+            wrong1: 2018,
+            wrong2: 2022,
+            wrong3: 2030,
+            correct: 2020
+        },
+        img: "../images/halloween2020.jpg"
+    },
+    q2: {
+        question: "In what country was the first account of children saying \"Trick-or-Treat\" on halloween?",
+        answers: {
+            wrong1: "USA",
+            wrong2: "Ireland",
+            wrong3: "Scotland",
+            correct: "Canada"
+        },
+        img: "../images/canadahalloween.jpg"
+    },
+    q3: {
+        question: "In what decade did Trick-or-Treating start gaining popularity in the USA?",
+        answers: {
+            wrong1: "70's",
+            wrong2: "50's",
+            wrong3: "40's",
+            correct: "30's"
+        },
+        img: "../images/30shalloween.jpg"
+    },
+    q4: {
+        question: "About how many children have been seriously injred or killed from poisoned candy received from Trick-or-Treating?",
+        answers: {
+            wrong1: 1200,
+            wrong2: 500,
+            wrong3: 100,
+            correct: 0
+        },
+        img: "../images/poisoncandy.jpg"
+    },
+    q5: {
+        question: "What were Jack O'lanterns originally made from?",
+        answers: {
+            wrong1: "Cantaloupe",
+            wrong2: "Beets",
+            wrong3: "Pumpkins",
+            correct: "Turnips"
+        },
+        img: "../images/turnip.jpg"
+    },
+    q6: {
+        question: "In what country did Jack O'lantern carving originate?",
+        answers: {
+            wrong1: "France",
+            wrong2: "Scotland",
+            wrong3: "USA",
+            correct: "Ireland"
+        },
+        img: "../images/ireland.jpg"
+    },
+    q7: {
+        question: "Pumpkins are native to what continent?",
+        answers: {
+            wrong1: "Asia",
+            wrong2: "Europe",
+            wrong3: "South America",
+            correct: "North America"
+        },
+        img: "../images/america.jpg"
+    },
+    q8: {
+        question: "How much are American consumers expected to spend on halloween in 2018?",
+        answers: {
+            wrong1: "20 Billion",
+            wrong2: "100 Million",
+            wrong3: "10 Million",
+            correct: "9 Billion"
+        },
+        img: "../images/spending.jpg"
+    },
+    q9: {
+        question: "What is the second most popular halloween candy?",
+        answers: {
+            wrong1: "Starburst",
+            wrong2: "Skittles",
+            wrong3: "Chocolate",
+            correct: "Candy Corn"
+        },
+        img: "../images/candycorn.jpg"
+    },
+    q10: {
+        question: "In what state is it illegal to dress up like a priest or nun?",
+        answers: {
+            wrong1: "California",
+            wrong2: "South Dakota",
+            wrong3: "North Carolina",
+            correct: "Alabama"
+        },
+        img: "../images/preistnun.jpg"
+    }
+}
